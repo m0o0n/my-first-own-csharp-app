c25dd2c546b23af87ccf3d04065719a086b1acd2
Vladyslav Shtrikun <strikunvlad@gmail.com>
2026-09-18 12:00:01 +0300

    remove no need code add directory Packages props

--- src/OrderTracking.Read/Program.cs
+++ src/OrderTracking.Read/Program.cs
@@ -1,4 +1,14 @@
+
+using Marten;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration["DB_URL"]!);
+    options.DatabaseSchemaName = "write_orders";
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
--- src/OrderTracking.Write/Program.cs
+++ src/OrderTracking.Write/Program.cs
@@ -1,54 +1,15 @@
-using Microsoft.AspNetCore.Hosting.Server;
-using Microsoft.AspNetCore.Hosting.Server.Features;
+using Marten;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration["DB_URL"]!);
+    options.DatabaseSchemaName = "write_orders";
+});
 
 var app = builder.Build();
 
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.Lifetime.ApplicationStarted.Register(() =>
-{
-    var addresses = app.Services.GetRequiredService<IServer>().Features.Get<IServerAddressesFeature>()!.Addresses;
-    foreach (var address in addresses)
-    {
-        app.Logger.LogInformation("Listening on {Address}", address);
-    }
-});
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
